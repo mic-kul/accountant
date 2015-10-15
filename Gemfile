8b9f03a7c326ee4fc7315a26d70d59dc8617219b
Michał Kulesza <michal.kulesza@netguru.co>
2015-10-15 18:10:49 +0200

    Use faktura_parser & wfirma_expenses_add gem

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'faktura_parser', git: 'git://github.com/mic-kul/faktura_parser'
+gem 'wfirma_expenses_add', git: 'git://github.com/mic-kul/wfirma_expenses_add'
+
--- ksiegowy.rb
+++ ksiegowy.rb
@@ -1,2 +1,13 @@
 require 'yaml'
+require 'rubygems'
+require 'faktura_parser'
+require 'wfirma_expenses_add'
+
 config = YAML.load_file('ksiegowy.yml')
+
+invoice_file_path = ARGV[0]
+
+faktura_parser = FakturaParser.new(invoice_file_path).to_hash
+#nip, document_number, amount_brutto, purchase_date
+Wfirma::Expenses::Add.new(config['wfirma']['login'], config['wfirma']['password'], faktura_parser[:nip], faktura_parser[:document_number], faktura_parser[:amount_brutto], faktura_parser[:date]).run!
+puts "faktura zaksiegowana"
